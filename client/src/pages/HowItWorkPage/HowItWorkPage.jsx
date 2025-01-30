9ab06340dd5ee5c136273afe1485d43f15cd4bd9
Karina <milkokarina13@gmail.com>
2025-01-30 22:31:59 +0200

    Add introContainer + styles

--- client/src/pages/HowItWorkPage/HowItWorkPage.jsx
+++ client/src/pages/HowItWorkPage/HowItWorkPage.jsx
@@ -1,5 +1,33 @@
+import React from 'react';
+import styles from './../HowItWorkPage/HowItWorkPage.module.sass';
+import CONSTANTS from './../../constants';
+
+const { STATIC_IMAGES_PATH } = CONSTANTS;
 function HowItWorkPage() {
-  return <div>HowItWorkPage</div>;
+  return (
+    <div className={styles.atomContainer}>
+      <div className={styles.introContainer}>
+        <section className={styles.introSection}>
+          <div className={styles.introTextBlock}>
+            <h4 className={styles.introSubtitle}>World's #1 Naming Platform</h4>
+            <h1 className={styles.introTitle}>How Does Atom Work?</h1>
+            <p className={styles.introDescription}>
+              Atom helps you come up with a great name for your business by
+              combining the power of crowdsourcing with sophisticated technology
+              and Agency-level validation services.
+            </p>
+          </div>
+          <iframe
+            className={styles.introVideo}
+            src="https://iframe.mediadelivery.net/embed/239474/327efcdd-b1a2-4891-b274-974787ae8362"
+            frameborder="0"
+            allow="autoplay; fullscreen; picture-in-picture"
+            allowfullscreen
+          ></iframe>
+        </section>
+      </div>
+    </div>
+  );
 }
 
 export default HowItWorkPage;
